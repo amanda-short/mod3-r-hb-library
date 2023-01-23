0336922d8201fc922228cdccd4e6d31f0e6a1841
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2023-01-23 15:43:00 -0800

    /books route added, secrets and environment  set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import BookDetail from './components/book/BookDetail.js';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route exact path="/books" component={BookList} />
+      </Switch>
     </main>
   );
 }
