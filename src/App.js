4bc68ae175bdb77d0f6316552d6ac0cfd4172740
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2023-01-23 16:05:49 -0800

    /books/:id route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import BookList from './components/book/BookList';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-
 import BookDetail from './components/book/BookDetail.js';
 
 function App() {
-  // TODO: Add routes to books & views
+  // TODO: Add routes to books
   return (
     <main className="container">
       <Switch>
         <Route exact path="/books" component={BookList} />
+        <Route exact path="/books/:id" component={BookDetail} />
       </Switch>
     </main>
   );
